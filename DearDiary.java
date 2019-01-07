d0ceeb9a36b22b6fe4c1df5ec29e5474c10637fa
Tamila Skakova <36418704+TamilaS@users.noreply.github.com>
2019-01-07 23:48:23 +0600

    Add files via upload

--- DearDiary.java
+++ DearDiary.java
@@ -16,7 +16,7 @@ public class DearDiary {
         Scanner scanner = new Scanner(System.in);
         int numberOfEntries = scanner.nextInt();
 
-        for(int i = 0; i < numberOfEntries; i++) {
+        for (int i = 0; i < numberOfEntries; i++) {
             DiaryEntry diaryEntry = new DiaryEntry();
             System.out.println("Now tell me your mood...");
             diaryEntry.setMood(scanner.next());
@@ -28,52 +28,54 @@ public class DearDiary {
 
         int moodOMeter = 0;
         System.out.println("Telling you your thoughts again...");
-        for(DiaryEntry diaryEntry : diaryEntries) {
+        for (DiaryEntry diaryEntry : diaryEntries) {
             System.out.println(diaryEntry);
-            if(diaryEntry.getMood().equalsIgnoreCase("Good")) {
+            if (diaryEntry.getMood().equalsIgnoreCase("Good")) {
                 moodOMeter++;
-            } else if(diaryEntry.getMood().equalsIgnoreCase("Bad"))
-            {
+            } else if (diaryEntry.getMood().equalsIgnoreCase("Bad")) {
                 moodOMeter--;
+            }
         }
-    }
 
-    System.out.println("Performing mood analysis...");
-    if(moodOMeter > 0) {
-        System.out.println("I am glad you feel good Tamila");
-    } else if(moodOMeter < 0) {
-        System.out.println("Bad mood happens, but you have to stay strong...");
+        System.out.println("Performing mood analysis...");
+        if (moodOMeter > 0) {
+            System.out.println("I am glad you feel good Tamila");
+        } else if (moodOMeter < 0) {
+            System.out.println("Bad mood happens, but you have to stay strong...");
         } else {
             System.out.println("No mood is always a food for thought");
+        }
     }
+
+    private class DiaryEntry {
+        private String diaryText;
+        private String mood;
+
+        public void setDiaryText(String diaryText) {
+            if (diaryText == null || diaryText.isEmpty()) {
+                throw new IllegalArgumentException("Sorry, can't handle empty/null text");
+            }
+            this.diaryText = diaryText;
         }
 
-        private class DiaryEntry {
-            private String diaryText;
-            private String mood;
-            public void setDiaryText(String diaryText) {
-                if(diaryText == null || diaryText.isEmpty()) {
-                    throw new IllegalArgumentException("Sorry, can't handle empty/null text");
-                }
-                this.diaryText = diaryText;
+        public String getDiaryText() {
+            return diaryText;
+        }
+
+        public void setMood(String mood) {
+            if (!mood.equalsIgnoreCase("Good") && !mood.equalsIgnoreCase("Bad")) {
+                throw new IllegalArgumentException("Sorry, you have to tell me how you feel");
             }
-            public String getDiaryText() {
-                return diaryText;
-            }
-            public void setMood(String mood) {
-                if(!mood.equalsIgnoreCase("Good") &&
-                        !mood.equalsIgnoreCase("Bad")) {
-                    throw new IllegalArgumentException("Sorry, you have to tell me how you feel");
-                }
-                this.mood = mood;
-            }
-            public String getMood() {
-                return mood;
-            }
-            @Override
-            public String toString() {
-                return "Mood:" + this.mood + " Thought: " +
-                        this.diaryText;
-            }
+            this.mood = mood;
+        }
+
+        public String getMood() {
+            return mood;
+        }
+
+        @Override
+        public String toString() {
+            return "Mood:" + this.mood + " Thought: " + this.diaryText;
         }
     }
+}
